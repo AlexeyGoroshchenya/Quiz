39eff038a89062ba32209e567c7fc935a8bd1368
Алексей Горощеня <lastpaladin13@gmail.com>
2023-11-25 21:45:02 +0300

    fonts optimization

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -1,6 +1,7 @@
 import { burger } from './modules/burger';
 import { nav } from './modules/nav';
 import { quiz } from './modules/quiz';
+import { asyncStyles } from './modules/asyncStyles';
 import { scroll } from './modules/scroll';
 import { formModals } from './modules/formModals';
 import { renderSlider } from './modules/renderSlider';
@@ -11,8 +12,8 @@ import { renderSlider } from './modules/renderSlider';
 
 burger()
 nav()
-
 quiz()
+asyncStyles()
 formModals()
 renderSlider()
 scroll()
---
+++ assets/js/src/modules/asyncStyles.js
@@ -0,0 +1,20 @@
+export const asyncStyles = () => {
+
+    const createLink = (url)=>{
+        let elem = document.createElement('link');
+    elem.rel = 'stylesheet';
+    elem.href = url
+    document.head.appendChild(elem);
+    }
+    
+    createLink('assets/css/reviews.css')
+    createLink('assets/css/modal.css')
+    createLink('assets/css/footer.css')
+    createLink('https://fonts.googleapis.com/css2?family=Raleway:wght@500;600;700&display=swap')
+
+
+
+
+
+    
+}
